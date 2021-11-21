95e2d28ff2652c0bb96a5be8a171e68e041b9cb2
Rodrigo <nobrega@gmail.com>
2021-11-21 21:54:17 +0800

    odinson 0.08
    seteditionurl()
    getsoup()
    seteditionslist()
    geteditionnumber() Ready OK

--- odinson.py
+++ odinson.py
@@ -3,9 +3,10 @@
 # Scrape Guia dos Quadrinhos webpages to collect original Marvel editions
 # from old magazines published in Brazil
 # Author: Rodrigo Nobrega
-# 20211114 20211116
+# 20211114 20211121
 #########################################################################################
-__version__ = 0.07
+__title__ = "Odinson"
+__version__ = 0.08
 
 
 # import libraries
@@ -24,23 +25,88 @@ class Odinson(object):
     """
     def __init__(self, initialurl):
         self.initialurl = initialurl
-        self.editionurl = self.geteditionurl()
+        self.editionurl = ""
+        self.pagesoup = None
+        self.editionslist = []
+        self.editionnumber = []
     
-    def geteditionurl(self):
+    def seteditionurl(self):
+        """Take the edition part of the whole URL"""
+        print(" Defining Edition URL")
         edu = self.initialurl.replace(URLROOT, "")
         return edu
+    
+    def getsoup(self):
+        """Retrieve the Beautiful Soup version of a page"""
+        URL = URLROOT + self.editionurl
+        print(" Retrieving page contents")
+        page = requests.get(URL)
+        thesoup = BeautifulSoup(page.content, "html.parser")
+        return thesoup
+           
+    def seteditionslist(self):
+        """Create a list of all editions URLs"""
+        outlist = []
+        print(" Retrieving all Editions URL list")
+        nav = self.pagesoup.find(id="d0")
+        for link in nav.find_all("a"):
+            outlist.append(f'{link["href"].replace("../", "")}')
+        return outlist
+    
+    def geteditionnumber(self):
+        """Takes the edition title and number"""
+        ed = self.pagesoup.find(id="nome_titulo_lb")
+        edl = ed.contents
+        edl = [f"{i}".strip() for i in edl]
+        edl = [i.split(">")[1] if ">" in i else i for i in edl]
+        edl = [i.split("<")[0] if "<" in i else i for i in edl]
+        edl = [i.replace("n°", "").strip() for i in edl if i != '']
+        singleed = [edl[0], edl[-1]]
+        return singleed      
+    
+    
         
 
 # main loop
 def main():
-    print('\n===========================================================================')
-    print('                                  Odinson')
-    print('===========================================================================\n')
+    # header --------------------------------------------------------------
+    print(f'\n{75 * "="}')
+    print(f'{f"{__title__} v.{__version__}":^75}')
+    print(f'{75 * "="}\n')
+    # ---------------------------------------------------------------------
+    # main code goes here
+    #
+    # footer --------------------------------------------------------------
+    print(f'\n{34 * "="}  OK  {35 * "="}\n')
+
+
+# test loop
+def test():
+    # header --------------------------------------------------------------
+    print(f'\n{75 * "="}')
+    print(f'{"TEST":^75}')
+    print(f'{f"{__title__} v.{__version__}":^75}')
+    print(f'{75 * "="}\n')
+    # ---------------------------------------------------------------------
     od = Odinson("http://www.guiadosquadrinhos.com/edicao/herois-da-tv-2-serie-n-1/htv0302/6274")
-    print(od.editionurl)
-    print('\n================================= THE END =================================\n')
+    # Set Edition URL
+    od.editionurl = od.seteditionurl()
+#     print(f" Edition URL: {od.editionurl}")
+    # Get page soup
+    od.pagesoup = od.getsoup()
+#     print(od.pagesoup)
+    # Define all Editions list
+    od.editionslist = od.seteditionslist()
+#     [print(i) for i in od.editionslist]
+    # Define Edition name and number
+    od.editionnumber = od.geteditionnumber()
+    print(f" Edition Number: {od.editionnumber}")
+    #
+    # footer --------------------------------------------------------------
+    print(f'\n{34 * "="}  OK  {35 * "="}\n')
 
 
 # main, calling main loop
 if __name__ == '__main__':
-    main()
+#     main()
+    test()
