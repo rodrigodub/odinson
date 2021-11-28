17a6cf5a3620e3ad0b36c55c77430fc48952c248
Rodrigo <nobrega@gmail.com>
2021-11-28 15:58:01 +0800

    odinson v0.12
    Updated attributes, getstorydetails(), getmaincharacter(), getoriginaltitle(), getoriginalnumber()

--- odinson.py
+++ odinson.py
@@ -3,10 +3,10 @@
 # Scrape Guia dos Quadrinhos webpages to collect original Marvel editions
 # from old magazines published in Brazil
 # Author: Rodrigo Nobrega
-# 20211114 / 20211126
+# 20211114 / 20211127
 #########################################################################################
 __title__ = "Odinson"
-__version__ = "0.11"
+__version__ = "0.12"
 
 
 # import libraries
@@ -20,7 +20,7 @@ URLROOT = "http://www.guiadosquadrinhos.com/"
 
 # Odinson Class
 class Odinson(object):
-    """
+    """x
     Class to find original Marvel editions from old magazines published in Brazil
     """
     def __init__(self, initialurl):
@@ -30,18 +30,22 @@ class Odinson(object):
         self.editionslist = self.seteditionslist()
         self.editionnumber = self.geteditionnumber()
         self.editioncontents = self.geteditioncontents()
-        self.editionstories = self.getallstories()
-#         self.allstories = []
-    
+        self.storytitles = self.getallstories()
+        self.storydetails = self.getstorydetails()
+        self.maincharacter = self.getmaincharacter()
+        self.originaltitle = self.getoriginaltitle()
+        self.originalnumber = self.getoriginalnumber()
+            
     def seteditionurl(self):
         """Take the edition part of the whole URL"""
         edu = self.initialurl.replace(URLROOT, "")
-        print(" Obtained Edition URL")
+#         print(" Obtained Edition URL")
         return edu
     
     def getsoup(self):
         """Retrieve the Beautiful Soup version of a page"""
         URL = URLROOT + self.editionurl
+        print(f" {45 * '='}")
         print(" Retrieving page contents")
         page = requests.get(URL)
         thesoup = BeautifulSoup(page.content, "html.parser")
@@ -50,7 +54,7 @@ class Odinson(object):
     def seteditionslist(self):
         """Create a list of all editions URLs"""
         outlist = []
-        print(" Retrieving all Editions URL list")
+#         print(" Retrieving all Editions URL list")
         nav = self.pagesoup.find(id="d0")
         for link in nav.find_all("a"):
             outlist.append(f'{link["href"].replace("../", "")}')
@@ -58,7 +62,7 @@ class Odinson(object):
     
     def geteditionnumber(self):
         """Takes the edition title and number"""
-        print(" -------------------------------------------")
+        print(f" {45 * '.'}")
         ed = self.pagesoup.find(id="nome_titulo_lb")
         edl = ed.contents
         edl = [f"{i}".strip() for i in edl]
@@ -67,7 +71,7 @@ class Odinson(object):
         edl = [i.replace("n°", "").strip() for i in edl if i != '']
         singleed = [edl[0], edl[-1]]
         print(f"  {singleed[0]} #{singleed[1]}")
-        print(" -------------------------------------------")
+        print(f" {45 * '.'}")
         return singleed
     
     def geteditioncontents(self):
@@ -78,16 +82,51 @@ class Odinson(object):
     
     def getallstories(self):
         """Return a list of all stories of an edition"""
-        storiesdict = {}
+        titlesdict = {}
         # page section
-        results = self.pagesoup.find(id="texgith to_pag_detalhe")
+        results = self.pagesoup.find(id="texto_pag_detalhe")
         # individual stories
         stories = results.find_all("div", class_="historia")
         for i in range(len(stories)):
-            storiesdict[i] = stories[i].text
-        return storiesdict
+            titlesdict[i] = stories[i].text
+        return titlesdict
+    
+    def getstorydetails(self):
+        """Create dictionary with each story contents"""
+        storydetdict = {}
+        for i in self.storytitles.keys():
+            if i < len(self.storytitles)-1:
+                storydetails = self.editioncontents.split(self.storytitles[i])[1].split(self.storytitles[i+1])[0]
+            else:
+                storydetails = self.editioncontents.split(self.storytitles[i])[1]
+            storydetdict[i] = storydetails
+        return storydetdict
+    
+    def getmaincharacter(self):
+        """Return the stories main character"""
+        chardict = {}
+        for i in self.storydetails.keys():
+            chardict[i] = self.storydetails[i].split(",")[0].split("Personagens:")[1].strip()
+        return chardict
+    
+    def getoriginaltitle(self):
+        """Return the stories original title"""
+        titledict = {}
+        for i in self.storydetails.keys():
+            titledict[i] = self.storydetails[i].split("Publicada originalmente em")[1].split("(")[0].strip()
+        return titledict
+    
+    def getoriginalnumber(self):
+        """Return the stories original number"""
+        numdict = {}
+        for i in self.storydetails.keys():
+            numdict[i] = self.storydetails[i].split("Publicada originalmente em")[1].split(")")[1].replace("n°\xa0", "").split("/")[0].strip()
+        return numdict
+    
+    def setdataframe(self):
+        pass
 
-
+    
 # Controller function
 def odinsoncontrol(firsteditionurl):
     """Function to drive the workflow"""
